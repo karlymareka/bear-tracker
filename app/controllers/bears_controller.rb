db326fc03870e1f56ef513752a44216a9a7e25e3
Karly Mareka <github email address>
2019-08-25 13:51:07 -0700

    added to bears controller

--- app/controllers/bears_controller.rb
+++ app/controllers/bears_controller.rb
@@ -9,4 +9,13 @@ class BearsController < ApplicationController
     erb :'/bears/new'
   end
 
+  post '/bears/new' do
+    @bear = Bear.create(:name => params[:name],
+      :sex => params[:sex],
+      :age => params[:age],
+      :health_status => params[:health_status],
+      :habituation_status => params[:habituation_status])
+    
+  end
+
 end
