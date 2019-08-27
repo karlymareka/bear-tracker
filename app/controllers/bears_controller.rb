56531917798ec43e2e708d02915f501a813f3142
Karly Mareka <github email address>
2019-08-26 19:38:36 -0700

    added bears show view

--- app/controllers/bears_controller.rb
+++ app/controllers/bears_controller.rb
@@ -6,20 +6,31 @@ class BearsController < ApplicationController
   end
 
   get '/bears/new' do
-    if session[user_id] == nil
+    if session[:user_id] == nil
       redirect to '/login'
     else
       erb :'/bears/new'
-    end 
+    end
   end
 
-  post '/bears/new' do
+  post '/bears' do
     @bear = Bear.create(:name => params[:name],
       :sex => params[:sex],
       :age => params[:age],
       :health_status => params[:health_status],
       :habituation_status => params[:habituation_status],
-      :ranger_id => session[user_id])
+      :ranger_id => session[:user_id])
+    redirect '/bears'
   end
+
+  get '/bears/:id' do
+   if session[:user_id] == nil
+     redirect to '/login'
+   else
+     @bear = Bear.find(params[:id])
+     @ranger = Ranger.find(@bear.ranger_id)
+     erb :'/bears/show'
+   end
+ end
 
 end
